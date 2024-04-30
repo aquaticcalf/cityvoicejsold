61a0c9c432ffd2a404d8d40d29a1806d1d3b8343
sam <aquaticcalf@proton.me>
2024-04-30 11:41:20 +0530

    meow

--- src/ui/outlinebutton.jsx
+++ src/ui/outlinebutton.jsx
@@ -1,6 +1,6 @@
 function Outlinebutton({children}) {
   return(
-    <div className="border-[2px] border-[#0047FF] font-bold text-[#0047FF] w-full max-w-[350px] h-[50px] flex justify-center items-center rounded-[5px]">
+    <div className="border-[2px] border-[#0047FF] font-bold text-[#0047FF] w-full max-w-[350px] h-[50px] flex justify-center items-center rounded-[5px] cursor-pointer">
       {children}
     </div>
   )
--- src/ui/solidbutton.jsx
+++ src/ui/solidbutton.jsx
@@ -1,6 +1,6 @@
 function Solidbutton({children}) {
   return(
-    <div className="bg-[#0047FF] w-full max-w-[350px] h-[50px] font-bold flex justify-center items-center rounded-[5px]">
+    <div className="bg-[#0047FF] w-full max-w-[350px] h-[50px] font-bold flex justify-center items-center rounded-[5px] cursor-pointer">
       {children}
     </div>
   )
